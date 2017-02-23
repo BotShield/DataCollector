17f772da346697ad597e4a37f7d8d7ced01f3fe1
Andreas Volz <Andreas Volz@AndreasVolz-PC.fritz.box>
2017-02-23 20:48:54 +0100

    Refactoring: Einfuehrung eines separaten Runners, der nun die public static void main enthaelt Prepared Statements in PGDBConnection nur noch einmal erstellen

--- src/main/java/de/botshield/CaptureFilterStream.java
+++ src/main/java/de/botshield/CaptureFilterStream.java
@@ -16,9 +16,7 @@
 
 package de.botshield;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.sql.SQLException;
 
 import twitter4j.FilterQuery;
 import twitter4j.StallWarning;
@@ -30,180 +28,83 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
 /**
- * Klasse zum Schreiben von Tweets in eine DB basierend auf Schlagwörtern.
+ * Klasse zum Schreiben von Tweets in eine DB basierend auf Schlagwoertern.
  */
 public final class CaptureFilterStream {
 
     private PGDBConnection dbConn;
-    private boolean blnWritetoDB = false;
-    private final static String PROPERTY_FILE = "dataCollector.properties";
-    private final static String PROPERTY_KEY_FOLLOW = "toBeFollowed";
-    private final static String PROPERTY_KEY_TOPICS = "trackTopics";
-    private final static String PROPERTY_SEPARATOR = ",";
-    private final static String PROPERTY_DATABASEINTEGRATION = "WriteToDatabase";
-
-    StatusListener listener = new StatusListener() {
-        @Override
-        public void onStatus(Status status) {
-            System.out.println("@" + status.getUser().getScreenName());
-            if (blnWritetoDB)
-                dbConn.insertStatus(status);
-        }
-
-        @Override
-        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-            System.err.println("Got a status deletion notice id:"
-                    + statusDeletionNotice.getStatusId());
-        }
-
-        @Override
-        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-            System.err.println("Got track limitation notice:"
-                    + numberOfLimitedStatuses);
-        }
-
-        @Override
-        public void onScrubGeo(long userId, long upToStatusId) {
-            System.err.println("Got scrub_geo event userId:" + userId
-                    + " upToStatusId:" + upToStatusId);
-        }
-
-        @Override
-        public void onStallWarning(StallWarning warning) {
-            System.err.println("Got stall warning:" + warning);
-        }
-
-        @Override
-        public void onException(Exception ex) {
-            // TODO: sauberer Abbau der DB Connection bei Programmende
-            // hier kein closeConnection, da jede SQLException sonst alle
-            // zukünftigen Schreibprozess unmöglich macht
-            // dbConn.closeConnection();
-            ex.printStackTrace();
-        }
-    };
+    private boolean blnWritetoDB;
+    private StatusListener listener;
 
     /*
      * Constructor
      */
     public CaptureFilterStream() {
+    }
 
-        /*
-         * Alternative Konfigurationsmethode ohne Properties-Datei
-         * ConfigurationBuilder cb = new ConfigurationBuilder();
-         * cb.setDebugEnabled(true)
-         * .setOAuthConsumerKey("*********************")
-         * .setOAuthConsumerSecret("******************************************")
-         * .setOAuthAccessToken(
-         * "**************************************************")
-         * .setOAuthAccessTokenSecret(
-         * "******************************************");
-         * 
-         * TwitterFactory tf = new TwitterFactory(cb.build()); Twitter twitter =
-         * tf.getInstance();
-         */
+    public void initializeStreamListener() {
+
+        listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                if (isBlnWritetoDB()) {
+                    System.out.println("*********************** Writing tweet into db! *********************** ");
+                    dbConn.insertStatus(status);
+                }
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.err.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.err.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.err.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.err.println("Got stall warning:" + warning);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                // TODO: sauberer Abbau der DB Connection bei Programmende
+                // hier kein closeConnection, da jede SQLException sonst alle
+                // zukuenftigen Schreibprozess unmoeglich macht
+                // dbConn.closeConnection();
+                ex.printStackTrace();
+            }
+        };
+
     }
 
     /**
-     * @return -1 if connection could not be established
+     * @return false if connection could not be established
+     * @throws SQLException
      */
-    public int setupConnection() {
+    public boolean setupConnection() throws SQLException {
         dbConn = new PGDBConnection();
-        return dbConn.establishConnection("jstrebel", "", "twitter"); // login
-        // meiner
-        // Testdatenbank
-        // auf localhost
+        boolean result = dbConn.establishConnection("dbUser", "dataCollector", "twitter");
+        if (result) {
+            dbConn.prepareStatements();
+        }
+        return result;
     }
 
-    public void execute(long[] followArray, String[] trackArray)
-            throws TwitterException {
+    public void execute(long[] followArray, String[] trackArray) throws TwitterException {
         TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
         twitterStream.addListener(listener);
 
         // filter() method internally creates a thread which manipulates
         // TwitterStream and calls these adequate listener methods continuously.
         twitterStream.filter(new FilterQuery(0, followArray, trackArray));
-    }
-
-    public static void main(String[] args) throws TwitterException {
-        Properties props = new Properties();
-        CaptureFilterStream objCapture = new CaptureFilterStream();
-
-        // Read definitions from property file
-        try {
-            InputStream is = objCapture.getClass().getClassLoader()
-                    .getResourceAsStream(PROPERTY_FILE);
-            props.load(is);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        long[] followArray = readFollow(props);
-        String[] trackArray = readTopics(props);
-
-        // Check if database integration is needed
-        if (props.getProperty(PROPERTY_DATABASEINTEGRATION).trim()
-                .equals("true")
-                && objCapture.setupConnection() != -1) {
-            objCapture.setBlnWritetoDB(true);
-        } else {
-            objCapture.setBlnWritetoDB(false);
-        }
-
-        // start following
-        objCapture.execute(followArray, trackArray);
-    }
-
-    /**
-     * Extracts the topics that are to be tracked from a property file. The
-     * topics are expected to be contained in the passed "props" as a
-     * comma-separated list of Strings.
-     *
-     * @param props
-     *            Contains the topics to be tracked.
-     * @return The topics to be tracked as an array of String.
-     */
-    private static String[] readTopics(Properties props) {
-        String[] trackArray = null;
-        String topicsToBeTracked = props.getProperty(PROPERTY_KEY_TOPICS);
-        if (topicsToBeTracked != null && !topicsToBeTracked.trim().isEmpty()) {
-            String[] topicsArray = topicsToBeTracked.split(PROPERTY_SEPARATOR);
-
-            // the track array may contain a maximum of 10 entries TODO: stimmt
-            // das?
-            trackArray = new String[Math.min(10, topicsArray.length)];
-            for (int n = 0; n < topicsArray.length; n++) {
-                String topicToBeTracked = topicsArray[n].trim();
-                trackArray[n] = topicToBeTracked.trim();
-            }
-
-        }
-        return trackArray;
-    }
-
-    /**
-     * Extracts the users that are to be followed from a property file. The
-     * users are expected to be contained in the passed "props" as
-     * comma-separated list of longs.
-     *
-     * @param props
-     *            Contains the users to be followed.
-     * @return The users to be followed as an array of long.
-     */
-    private static long[] readFollow(Properties props) {
-        long[] followArray = null;
-        String usersToBeFollowed = props.getProperty(PROPERTY_KEY_FOLLOW);
-        if (usersToBeFollowed != null && !usersToBeFollowed.trim().isEmpty()) {
-            String[] usersArray = usersToBeFollowed.split(PROPERTY_SEPARATOR);
-            // the follow array may contain a maximum of 10 entries TODO: stimmt
-            // das?
-            followArray = new long[Math.min(10, usersArray.length)];
-            for (int n = 0; n < followArray.length; n++) {
-                String userToBeFollowed = usersArray[n].trim();
-                followArray[n] = Long.parseLong(userToBeFollowed);
-            }
-        }
-        return followArray;
     }
 
     public boolean isBlnWritetoDB() {
--- src/main/java/de/botshield/PGDBConnection.java
+++ src/main/java/de/botshield/PGDBConnection.java
@@ -22,7 +22,12 @@ import twitter4j.User;
  */
 
 public class PGDBConnection {
-    private Connection db = null;
+    private Connection db;
+
+    private PreparedStatement stInsStatus;
+    private PreparedStatement stInsUser;
+    private PreparedStatement stInsURL;
+    private PreparedStatement stInsPlace;
 
     /**
      * Establishes a DB connection to a PostgreSQL DB.
@@ -33,32 +38,57 @@ public class PGDBConnection {
      *            the password
      * @param strDB
      *            the DB name
-     * @return 0 for success, -1 for failure
+     * @return true for success, false for failure
      */
-    public int establishConnection(String strUser, String strPW, String strDB) {
-        String url = "jdbc:postgresql://localhost/" + strDB;
-        Statement st;
+    public boolean establishConnection(String strUser, String strPW, String strDB) {
+        String url = "jdbc:postgresql:" + strDB;
 
         try {
             Class.forName("org.postgresql.Driver");
+            System.out.println("Trying to get connection to db with URL " + url);
             db = DriverManager.getConnection(url, strUser, strPW);
 
-            st = db.createStatement();
-            // ResultSet rs = st.executeQuery("SELECT * FROM \"T_TW_USERS\"");
+            Statement st = db.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM T_User");
-            System.out.println(rs.toString());
+            System.out.println("Result set: " + rs.toString());
 
             rs.close();
             st.close();
 
             db.setAutoCommit(false);
 
-            return 0;
+            return true;
         } catch (Exception e) {
-            System.err.println(e.toString());
-            return -1;
+            e.printStackTrace(System.err);
+            return false;
         }
     }// establishConnection
+
+    public void prepareStatements() throws SQLException {
+        String strInsStatus = "insert into T_Status(ID,status_Text,created_at, "
+                + "favourites_count,username,screen_name,lang,withheld_in_countries,"
+                + "InReplyToScreenName,InReplyToStatusId,"
+                + "InReplyToUserId,quoted_status_id,RetweetCount,retweeted_status_id,status_source,isFavorited,"
+                + "isPossiblySensitive,isRetweet,isRetweeted,isRetweetedByMe,isTruncated,recorded_at,status_user_id,latitude,longitude,status_place_id) "
+                + "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String strInsUser = "insert into T_User(ID,recorded_at," + "username,screen_name,created_at,description,"
+                + "geo_enabled,lang,followers_count,favourites_count,friends_count, listed_count,loca,"
+                + "statuses_count,TimeZone,user_URL,UtcOffset,WithheldInCountries,isContributorsEnabled,"
+                + "isDefaultProfile,isDefaultProfileImage,isFollowRequestSent,isProfileBackgroundTiled,"
+                + "isProfileUseBackgroundImage,isProtected,isTranslator,isverified,URLEntity_id) "
+                + "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        String strInsURL = "insert into T_URL(ID,recorded_at,display_url,expanded_url,"
+                + "indices_start,indices_end,url,urltext,entity_id) values (?,?,?,?,?,?,?,?,?)";
+
+        String strInsPlace = "insert into T_Place(ID,pname,pfullname,place_url,bb_type,geo_type,country,country_code,"
+                + "place_type,street_address,contained_place_id) values (?,?,?,?,?,?,?,?,?,?,?)";
+
+        stInsPlace = db.prepareStatement(strInsPlace);
+        stInsURL = db.prepareStatement(strInsURL);
+        stInsUser = db.prepareStatement(strInsUser);
+        stInsStatus = db.prepareStatement(strInsStatus);
+    }
 
     /**
      * Method for writing the data collection parameters to the database. It
@@ -70,22 +100,20 @@ public class PGDBConnection {
      *            REST or Streaming API
      * @return parameter ID
      */
-    public int registerDataCollectionParameter(String strTopics,
-            String strDatasource) {
+    public int registerDataCollectionParameter(String strTopics, String strDatasource) {
         PreparedStatement stInsDCPARAM = null;
-        String strInsDCPARAM = "insert into T_DataCollParameter(ID,track_topics,datasource) "
-                + "values (?,?,?)";
+        String strInsDCPARAM = "insert into T_DataCollParameter(ID,track_topics,datasource) " + "values (?,?,?)";
         try {
             stInsDCPARAM = db.prepareStatement(strInsDCPARAM);
             /*
-             * // hole Sequenznummer für die Session Statement st =
+             * // hole Sequenznummer fuer die Session Statement st =
              * db.createStatement(); ResultSet rs =
              * st.executeQuery("select nextval('param_seq')"); rs.next(); long
              * lPARAMid = rs.getLong(1); rs.close(); st.close();
-             * 
+             *
              * stInsDCPARAM.setLong(1, lPARAMid); stInsDCPARAM.setString(2,
              * strTopics);
-             * 
+             *
              * stInsDCPARAM.executeUpdate(); db.commit(); stInsDCPARAM.close();
              */
             // TODO: aaociate Status with param ID
@@ -113,33 +141,8 @@ public class PGDBConnection {
      * @return 0 for success, -1 for failure
      */
     public int insertStatus(Status twStatus) {
-        PreparedStatement stInsStatus = null;
-        String strInsStatus = "insert into T_Status(ID,status_Text,created_at, "
-                + "favourites_count,username,screen_name,lang,withheld_in_countries,"
-                + "InReplyToScreenName,InReplyToStatusId,"
-                + "InReplyToUserId,quoted_status_id,RetweetCount,retweeted_status_id,status_source,isFavorited,"
-                + "isPossiblySensitive,isRetweet,isRetweeted,isRetweetedByMe,isTruncated,recorded_at,status_user_id,latitude,longitude,status_place_id) "
-                + "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-        PreparedStatement stInsUser = null;
-        String strInsUser = "insert into T_User(ID,recorded_at,"
-                + "username,screen_name,created_at,description,"
-                + "geo_enabled,lang,followers_count,favourites_count,friends_count, listed_count,loca,"
-                + "statuses_count,TimeZone,user_URL,UtcOffset,WithheldInCountries,isContributorsEnabled,"
-                + "isDefaultProfile,isDefaultProfileImage,isFollowRequestSent,isProfileBackgroundTiled,"
-                + "isProfileUseBackgroundImage,isProtected,isTranslator,isverified,URLEntity_id) "
-                + "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
-        PreparedStatement stInsURL = null;
-        String strInsURL = "insert into T_URL(ID,recorded_at,display_url,expanded_url,"
-                + "indices_start,indices_end,url,urltext,entity_id) values (?,?,?,?,?,?,?,?,?)";
-
-        PreparedStatement stInsPlace = null;
-        String strInsPlace = "insert into T_Place(ID,pname,pfullname,place_url,bb_type,geo_type,country,country_code,"
-                + "place_type,street_address,contained_place_id) values (?,?,?,?,?,?,?,?,?,?,?)";
-
-        Timestamp tsrecorded_at = new Timestamp(Calendar.getInstance()
-                .getTimeInMillis());
+        Timestamp tsrecorded_at = new Timestamp(Calendar.getInstance().getTimeInMillis());
         long lURLid = -1;
         long lPlaceid = -1;
 
@@ -147,15 +150,13 @@ public class PGDBConnection {
 
             // Schreibe Place Objekt
             if (twStatus.getPlace() != null) {
-                // TODO: mache db.prepareStatement() nur einmal pro Statement,
-                // nicht bei jedem Methodenaufruf
-                stInsPlace = db.prepareStatement(strInsPlace);
 
-                Place twPlace = null;
-                twPlace = twStatus.getPlace();
+                Place twPlace = twStatus.getPlace();
 
-                // hole Sequenznummer für den Place
+                // hole Sequenznummer fuer den Place
                 Statement st = db.createStatement();
+                // TODO: kann man das nicht auch direkt in das Prepared
+                // statement eingeben?
                 ResultSet rs = st.executeQuery("select nextval('place_seq')");
                 rs.next();
                 lPlaceid = rs.getLong(1);
@@ -172,48 +173,42 @@ public class PGDBConnection {
                 stInsPlace.setString(8, twPlace.getCountryCode());
                 stInsPlace.setString(9, twPlace.getPlaceType());
                 stInsPlace.setString(10, twPlace.getStreetAddress());
-                // TODO: rekursive Auflösung verschachtelter Places
+                // TODO: rekursive Aufloesung verschachtelter Places
                 stInsPlace.setNull(11, Types.BIGINT);
 
                 stInsPlace.executeUpdate();
             }
 
             // schreibe URL-Objekt
-            if (twStatus.getUser() != null)
-                if (twStatus.getUser().getURLEntity() != null) {
-                    // TODO: mache db.prepareStatement() nur einmal pro
-                    // Statement,
-                    // nicht bei jedem Methodenaufruf
-                    stInsURL = db.prepareStatement(strInsURL);
-                    URLEntity twURL = null;
-                    twURL = twStatus.getUser().getURLEntity();
+            if (twStatus.getUser() != null && twStatus.getUser().getURLEntity() != null) {
+                URLEntity twURL = null;
+                twURL = twStatus.getUser().getURLEntity();
 
-                    // hole Sequenznummer für die URL
-                    Statement st = db.createStatement();
-                    ResultSet rs = st.executeQuery("select nextval('url_seq')");
-                    rs.next();
-                    lURLid = rs.getLong(1);
-                    rs.close();
-                    st.close();
-                    // setze Parameter
-                    stInsURL.setLong(1, lURLid);
-                    stInsURL.setTimestamp(2, tsrecorded_at);
-                    stInsURL.setString(3, twURL.getDisplayURL());
-                    stInsURL.setString(4, twURL.getExpandedURL());
-                    stInsURL.setInt(5, twURL.getStart());
-                    stInsURL.setInt(6, twURL.getEnd());
-                    stInsURL.setString(7, twURL.getURL());
-                    stInsURL.setString(8, twURL.getText());
-                    stInsURL.setNull(9, Types.BIGINT); // TODO: entity_id bigint
-                    // noch zu setzen, wenn
-                    // Entities implementiert
-                    // sind.
-                    // REFERENCES T_Entity(ID)
-                    stInsURL.executeUpdate();
-                }
+                // hole Sequenznummer fuer die URL
+                Statement st = db.createStatement();
+                ResultSet rs = st.executeQuery("select nextval('url_seq')");
+                rs.next();
+                lURLid = rs.getLong(1);
+                rs.close();
+                st.close();
+                // setze Parameter
+                stInsURL.setLong(1, lURLid);
+                stInsURL.setTimestamp(2, tsrecorded_at);
+                stInsURL.setString(3, twURL.getDisplayURL());
+                stInsURL.setString(4, twURL.getExpandedURL());
+                stInsURL.setInt(5, twURL.getStart());
+                stInsURL.setInt(6, twURL.getEnd());
+                stInsURL.setString(7, twURL.getURL());
+                stInsURL.setString(8, twURL.getText());
+                stInsURL.setNull(9, Types.BIGINT); // TODO: entity_id bigint
+                // noch zu setzen, wenn
+                // Entities implementiert
+                // sind.
+                // REFERENCES T_Entity(ID)
+                stInsURL.executeUpdate();
+            }
 
             // schreibe User-Objekt
-            stInsUser = db.prepareStatement(strInsUser);
             User twUser = null;
             if (twStatus.getUser() != null) {
                 twUser = twStatus.getUser();
@@ -222,8 +217,7 @@ public class PGDBConnection {
                 stInsUser.setTimestamp(2, tsrecorded_at);
                 stInsUser.setString(3, twUser.getName());
                 stInsUser.setString(4, twUser.getScreenName());
-                stInsUser.setTimestamp(5, new Timestamp(twUser.getCreatedAt()
-                        .getTime()));
+                stInsUser.setTimestamp(5, new Timestamp(twUser.getCreatedAt().getTime()));
                 stInsUser.setString(6, twUser.getDescription());
                 stInsUser.setInt(7, (twUser.isGeoEnabled() ? 1 : 0));
                 stInsUser.setString(8, twUser.getLang());
@@ -239,20 +233,16 @@ public class PGDBConnection {
                 stInsUser.setInt(17, twUser.getUtcOffset());
 
                 if (twUser.getWithheldInCountries() != null) {
-                    stInsUser.setString(18, twUser.getWithheldInCountries()
-                            .toString());
+                    stInsUser.setString(18, twUser.getWithheldInCountries().toString());
                 } else {
                     stInsUser.setNull(18, Types.VARCHAR);
                 }
-                ;
                 stInsUser.setInt(19, (twUser.isContributorsEnabled() ? 1 : 0));
                 stInsUser.setInt(20, (twUser.isDefaultProfile() ? 1 : 0));
                 stInsUser.setInt(21, (twUser.isDefaultProfileImage() ? 1 : 0));
                 stInsUser.setInt(22, (twUser.isFollowRequestSent() ? 1 : 0));
-                stInsUser.setInt(23,
-                        (twUser.isProfileBackgroundTiled() ? 1 : 0));
-                stInsUser.setInt(24, (twUser.isProfileUseBackgroundImage() ? 1
-                        : 0));
+                stInsUser.setInt(23, (twUser.isProfileBackgroundTiled() ? 1 : 0));
+                stInsUser.setInt(24, (twUser.isProfileUseBackgroundImage() ? 1 : 0));
                 stInsUser.setInt(25, (twUser.isProtected() ? 1 : 0));
                 stInsUser.setInt(26, (twUser.isTranslator() ? 1 : 0));
                 stInsUser.setInt(27, (twUser.isVerified() ? 1 : 0));
@@ -262,23 +252,20 @@ public class PGDBConnection {
                     stInsUser.setNull(28, Types.BIGINT);
                 }
                 stInsUser.executeUpdate();
+
             }
 
             // schreibe Status-objekt
-
-            stInsStatus = db.prepareStatement(strInsStatus);
             stInsStatus.setLong(1, twStatus.getId());
             stInsStatus.setString(2, twStatus.getText());
-            stInsStatus.setTimestamp(3, new Timestamp(twStatus.getCreatedAt()
-                    .getTime()));
+            stInsStatus.setTimestamp(3, new Timestamp(twStatus.getCreatedAt().getTime()));
             stInsStatus.setInt(4, twStatus.getFavoriteCount());
             stInsStatus.setString(5, twStatus.getUser().getName());
             stInsStatus.setString(6, twStatus.getUser().getScreenName());
             stInsStatus.setString(7, twStatus.getLang());
 
             if (twStatus.getWithheldInCountries() != null) {
-                stInsStatus.setString(8, twStatus.getWithheldInCountries()
-                        .toString());
+                stInsStatus.setString(8, twStatus.getWithheldInCountries().toString());
             } else {
                 stInsStatus.setNull(8, Types.VARCHAR);
             }
@@ -310,10 +297,8 @@ public class PGDBConnection {
 
             // Geolocation des Tweets
             if (twStatus.getGeoLocation() != null) {
-                stInsStatus.setDouble(24, twStatus.getGeoLocation()
-                        .getLatitude());
-                stInsStatus.setDouble(25, twStatus.getGeoLocation()
-                        .getLongitude());
+                stInsStatus.setDouble(24, twStatus.getGeoLocation().getLatitude());
+                stInsStatus.setDouble(25, twStatus.getGeoLocation().getLongitude());
             } else {
                 stInsStatus.setNull(24, Types.DOUBLE);
                 stInsStatus.setNull(25, Types.DOUBLE);
@@ -328,17 +313,8 @@ public class PGDBConnection {
             stInsStatus.executeUpdate();
 
             db.commit();
-            if (stInsStatus != null)
-                stInsStatus.close();
-            if (stInsUser != null)
-                stInsUser.close();
-            if (stInsURL != null)
-                stInsURL.close();
-            if (stInsPlace != null)
-                stInsPlace.close();
             return 0;
         } catch (SQLException e) {
-            // System.err.println(e.toString());
             e.printStackTrace();
             System.err.println(e.getSQLState());
             if (db != null) {
@@ -358,7 +334,7 @@ public class PGDBConnection {
                 }
             } // if
             return -1;
-        }// catch
+        } // catch
     }// insertStatus
 
     public int closeConnection() {
