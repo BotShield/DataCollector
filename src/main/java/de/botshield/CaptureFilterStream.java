9b5c6b8d1711edb74156f956491161fe80d18f51
Jörg Strebel <joerg.strebel@gmail.com>
2017-02-24 22:39:58 +0100

    Fixed botched merge and changed pom.xml details

--- src/main/java/de/botshield/CaptureFilterStream.java
+++ src/main/java/de/botshield/CaptureFilterStream.java
@@ -23,7 +23,6 @@ import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
-import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
@@ -105,87 +104,6 @@ public final class CaptureFilterStream {
         // filter() method internally creates a thread which manipulates
         // TwitterStream and calls these adequate listener methods continuously.
         twitterStream.filter(new FilterQuery(0, followArray, trackArray));
-    }
-
-    public static void main(String[] args) throws TwitterException {
-        Properties props = new Properties();
-        CaptureFilterStream objCapture = new CaptureFilterStream();
-
-        // Read definitions from property file
-        try {
-            InputStream is = objCapture.getClass().getClassLoader()
-                    .getResourceAsStream(PROPERTY_FILE);
-            props.load(is);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        long[] followArray = readFollow(props);
-        String[] trackArray = readTopics(props);
-
-        // Check if database integration is needed
-        if (props.getProperty(PROPERTY_DATABASEINTEGRATION).trim()
-                .equals("true")
-                && objCapture.setupConnection() != -1) {
-            objCapture.setBlnWritetoDB(true);
-        } else {
-            objCapture.setBlnWritetoDB(false);
-        }
-
-        // start following
-        objCapture.execute(followArray, trackArray);
-    }
-
-    /**
-     * Extracts the topics that are to be tracked from a property file. The
-     * topics are expected to be contained in the passed "props" as a
-     * comma-separated list of Strings.
-     *
-     * @param props
-     *            Contains the topics to be tracked.
-     * @return The topics to be tracked as an array of String.
-     */
-    private static String[] readTopics(Properties props) {
-        String[] trackArray = null;
-        String topicsToBeTracked = props.getProperty(PROPERTY_KEY_TOPICS);
-        if (topicsToBeTracked != null && !topicsToBeTracked.trim().isEmpty()) {
-            String[] topicsArray = topicsToBeTracked.split(PROPERTY_SEPARATOR);
-
-            // the track array may contain a maximum of 10 entries TODO: stimmt
-            // das?
-            trackArray = new String[Math.min(10, topicsArray.length)];
-            for (int n = 0; n < topicsArray.length; n++) {
-                String topicToBeTracked = topicsArray[n].trim();
-                trackArray[n] = topicToBeTracked.trim();
-            }
-
-        }
-        return trackArray;
-    }
-
-    /**
-     * Extracts the users that are to be followed from a property file. The
-     * users are expected to be contained in the passed "props" as
-     * comma-separated list of longs.
-     *
-     * @param props
-     *            Contains the users to be followed.
-     * @return The users to be followed as an array of long.
-     */
-    private static long[] readFollow(Properties props) {
-        long[] followArray = null;
-        String usersToBeFollowed = props.getProperty(PROPERTY_KEY_FOLLOW);
-        if (usersToBeFollowed != null && !usersToBeFollowed.trim().isEmpty()) {
-            String[] usersArray = usersToBeFollowed.split(PROPERTY_SEPARATOR);
-            // the follow array may contain a maximum of 10 entries TODO: stimmt
-            // das?
-            followArray = new long[Math.min(10, usersArray.length)];
-            for (int n = 0; n < followArray.length; n++) {
-                String userToBeFollowed = usersArray[n].trim();
-                followArray[n] = Long.parseLong(userToBeFollowed);
-            }
-        }
-        return followArray;
     }
 
     public boolean isBlnWritetoDB() {
